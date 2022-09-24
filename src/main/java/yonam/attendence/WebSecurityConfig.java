8e5cf09920db1f86c67100c11d08ab989e9c9071
RU-Developer <dev.ru@kakao.com>
2022-09-24 21:11:29 +0900

    로그아웃 오류 수정 Spring security logout filter 제거

--- src/main/java/yonam/attendence/WebSecurityConfig.java
+++ src/main/java/yonam/attendence/WebSecurityConfig.java
@@ -19,6 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.logout().disable();
+
         http.cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
