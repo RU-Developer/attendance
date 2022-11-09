3fd6685d21e2d30461318513c33d7f16596db397
RU-Developer <dev.ru@kakao.com>
2022-11-09 09:12:20 +0900

    LoginController RequestBody 적용

--- src/main/java/yonam/attendence/web/login/LoginController.java
+++ src/main/java/yonam/attendence/web/login/LoginController.java
@@ -50,7 +50,7 @@ public class LoginController {
 
     @ResponseBody
     @PostMapping(path = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public LoginResult loginAndroid(@Validated LoginForm form,
+    public LoginResult loginAndroid(@Validated @RequestBody LoginForm form,
                                     BindingResult bindingResult, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
